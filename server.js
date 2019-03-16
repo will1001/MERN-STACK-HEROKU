9e63460cf1d0a64e05ab1d393b80eb3d2fc332dc
will1001 <wilirahmatm@gmail.com>
2019-03-16 10:31:37 +0800

    text heroku 1

--- server.js
+++ server.js
@@ -1,42 +1,35 @@
 const express = require('express');
-const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const path = require('path');
-const port = process.env.PORT || 5000;
-const items = require('./Routes/api/items');
 const app = express();
+const port = process.env.PORT || 5000;
 
-
-//BodyParser Middleware
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-//DB config
 
-const db = require('./config/keys').mongoURI;
-
-// Connect to Mongo
-
-mongoose.connect(db)
-	.then(()=> console.log('connected . . .'))
-	.catch(err => console.log(err));
-
-	//Use Routes
-
-	app.use('/api/items',items);
+// API calls
+app.get('/api/hello', (req, res) => {
+  res.send({ express: 'Hello From Express' });
+});
+app.post('/api/world', (req, res) => {
+  console.log(req.body);
+  res.send(
+    `I received your POST request. This is what you sent me: ${req.body.post}`,
+  );
+});
 
 
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, 'client/build')));
+  // Handle React routing, return all requests to React app
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+}
 
-	if (process.env.NODE_ENV === 'production') {
-	  // Serve any static files
-	  app.use(express.static(path.join(__dirname, 'client/build')));
-	  // Handle React routing, return all requests to React app
-	  app.get('*', function(req, res) {
-	    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-	  });
-	}
 
-	app.listen(port,()=> console.log('Server started on port :' + port));
-
+app.listen(port, () => console.log(`Listening on port ${port}`));
